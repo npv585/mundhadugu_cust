a7a0d2706a547b3d24a0db52ebb61bc37d1e4d87
vamsikrishna <vamsikrishna.r@teleparadigm.com>
2014-03-26 17:36:16 +0530

    commit all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MundhaduguCust::Application.config.secret_key_base = '18b2ce40a786453a9ffd6586cd47ddac679075236e33262d9b4656f0b0b7898ed314b0696ed026fb21cc9e49791b63fb4eeff04c8ff59c00672cd987412f2bed'
